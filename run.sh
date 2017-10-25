1c74b48bdc53ad59886f70a6f99019e4cc17e0ee
Fin Christensen <christensen.fin@gmail.com>
2017-10-25 15:59:50 +0200

    Add hostname to run script

--- run.sh
+++ run.sh
@@ -1,7 +1,11 @@
 #! /bin/bash
 
+docker rm git-wetty || true
 docker stop git-wetty || true
 
-docker build -t git-wetty .
+if [[ "$1" == "rebuild" ]]
+then
+    docker build -t git-wetty .
+fi
 
-docker run --name git-wetty -p 127.0.0.1:4123:4123 -dt git-wetty
+docker run --rm -h ovgu --name git-wetty -p 127.0.0.1:4123:4123 -dt git-wetty
